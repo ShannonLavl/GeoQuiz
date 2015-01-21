11c4e17e05462f98e8349c86353bec8a676ca86b
Shannon Lavelle <lavelles@susqu.edu>
2015-01-20 21:55:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lewisjg/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package com.example.lewisjg.geoquiz;
+
+/**
+ * Created by lavelles on 1/20/2015.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
